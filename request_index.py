982861e7ef330c62a51993e132c7f0e26d30833f
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 20:32:10 -0700

    Update request_index.py

--- request_index.py
+++ request_index.py
@@ -9,7 +9,7 @@ def check_is_valid(package):
     return requests.get(f"https://pypi.org/pypi/{package}/json").json()["info"]["downloads"]["last_month"] >= 1
   except:
     return False
-packages = "\n".join([package[0] for package in r if ":" not in package[0] and not any(i in package[0] for i in excluded) and check_is_valid(package[0])][:10000])
+packages = "\n".join([package[0] for package in r if ":" not in package[0] and not any(i in package[0] for i in excluded) and check_is_valid(package[0])][:1000])
 
 with open("requirements.txt", "w") as f:
   f.write(packages)

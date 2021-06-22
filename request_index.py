0eb4f497c955e36acbf1cfaa8d46d19f5c5e9303
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 19:57:53 -0700

    Update request_index.py

--- request_index.py
+++ request_index.py
@@ -1,7 +1,7 @@
 import requests
 
 r = requests.get("https://www.piwheels.org/packages.json").json()
-packages = "\n".join([package[0] for package in r if ":" not in package[0][:10000])
+packages = "\n".join([package[0] for package in r if ":" not in package[0]][:10000])
 
 with open("requirements.txt", "w") as f:
   f.write(packages)

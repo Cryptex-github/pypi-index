7531a0de48aba597505e060e21c81d7b2ec15389
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 20:25:26 -0700

    Update request_index.py

--- request_index.py
+++ request_index.py
@@ -4,7 +4,7 @@ r = requests.get("https://www.piwheels.org/packages.json").json()
 excluded = [
   "mapnik",
 ]
-packages = "\n".join([package[0] for package in r if ":" not in package[0] and not any(i in package[0] for i in excluded)][:10000])
+packages = "\n".join([package[0] for package in r if ":" not in package[0] and not any(i in package[0] for i in excluded) and requests.get(f"https://pypi.org/pypi/{package[0]}/json").json()["downloads"]["last_month"] >= 1][:10000])
 
 with open("requirements.txt", "w") as f:
   f.write(packages)

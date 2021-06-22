fa4dd8946efbec5aec9429192fa344871fcc9dfa
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-06-21 20:29:44 -0700

    Update request_index.py

--- request_index.py
+++ request_index.py
@@ -4,7 +4,12 @@ r = requests.get("https://www.piwheels.org/packages.json").json()
 excluded = [
   "mapnik",
 ]
-packages = "\n".join([package[0] for package in r if ":" not in package[0] and not any(i in package[0] for i in excluded) and requests.get(f"https://pypi.org/pypi/{package[0]}/json").json()["info"]["downloads"]["last_month"] >= 1][:10000])
+def check_is_valid(package):
+  try:
+    return requests.get(f"https://pypi.org/pypi/{package}/json").json()["info"]["downloads"]["last_month"] >= 1
+  except:
+    return False
+packages = "\n".join([package[0] for package in r if ":" not in package[0] and not any(i in package[0] for i in excluded) and check_is_valid(package[0])][:10000])
 
 with open("requirements.txt", "w") as f:
   f.write(packages)
